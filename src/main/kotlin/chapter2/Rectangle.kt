9e78f5220f248ab3fbd87e9c9fd701da34ac14d1
woody.b <woody.b@kakaoenterprise.com>
2022-04-11 23:16:44 +0900

    chapter2

---
+++ src/main/kotlin/chapter2/Rectangle.kt
@@ -0,0 +1,18 @@
+package chapter2
+
+import java.util.Random
+
+class Rectangle(private val height: Int, val width: Int) {
+    val isSquare: Boolean
+        get() {
+            return height == width
+        }
+}
+
+fun createRandomRectangle() : Rectangle {
+    val random = Random()
+    return Rectangle(random.nextInt(), random.nextInt())
+}
+fun main() {
+    println(createRandomRectangle().isSquare)
+}
