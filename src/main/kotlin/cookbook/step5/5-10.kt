c2acf8cea78b5a825541ead7b46ef962664b405b
baekseungyeol <beck33333@naver.com>
2022-09-20 00:09:08 +0900

    cook book 5

--- src/main/kotlin/cookbook/step5/5-10.kt
+++ src/main/kotlin/cookbook/step5/5-10.kt
@@ -1,5 +1,37 @@
 package cookbook.step5
 
+
+interface Iterator<out T> {
+    operator fun next(): T
+    operator fun hasNext(): Boolean
+}
+
+data class Player(val name: String)
+class Team(
+    val name: String,
+    val players: MutableList<Player> = mutableListOf()) : Iterable<Player>{
+    fun addPlayers(vararg people: Player) = players.addAll(people)
+
+    override fun iterator(): kotlin.collections.Iterator<Player> {
+        return players.iterator()
+    }
+}
 fun main() {
+    val team = Team("Warriors")
+    team.addPlayers(
+        Player("Curry"),
+        Player("Thompson"),
+        Player("Durant"),
+        Player("Green"),
+        Player("Cousins"),
+    )
+
+    for(player in team) {
+        println(player)
+    }
+
+    println(team.joinToString { it.name })
+
+
 
 }
