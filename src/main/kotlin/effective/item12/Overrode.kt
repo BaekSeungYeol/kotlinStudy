6632a7829e4e2160abab1fe22cdc4da31ac7fff9
baekseungyeol <beck33333@naver.com>
2022-11-30 21:36:47 +0900

    effective

---
+++ src/main/kotlin/effective/item12/Overrode.kt
@@ -0,0 +1,10 @@
+package effective.item12
+
+fun Int.factorial(): Int = (1..this).product()
+
+fun Iterable<Int>.product(): Int =
+    fold(1) { acc, i -> acc * i}
+
+fun main() {
+    println(10 * 6.factorial())
+}
